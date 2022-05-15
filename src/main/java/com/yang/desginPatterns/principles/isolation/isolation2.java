783be2a7e6934668b1ec828e4e7950f457767f2a
yang <1741536475@qq.com>
2022-05-15 22:47:03 +0800

    isolation2 fist commit

--- src/main/java/com/yang/desginPatterns/principles/isolation/isolation2.java
+++ src/main/java/com/yang/desginPatterns/principles/isolation/isolation2.java
@@ -1,11 +1,17 @@
 package com.yang.desginPatterns.principles.isolation;
-
+//接口隔离原则
 public class isolation2 {
     public static void main(String[] args) {
         A a = new A();
         a.depend1(new B());//A类通过接口依赖B
         a.depend2(new B());//A类通过接口依赖B
         a.depend3(new B());//A类通过接口依赖B
+
+        C c = new C();
+        c.depend1(new D());//C类通过接口依赖D
+        c.depend4(new D());//C类通过接口依赖D
+        c.depend5(new D());//C类通过接口依赖D
+
     }
 }
 //接口1
